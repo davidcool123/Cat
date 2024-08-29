4450f21e7dc675dc6b4229b4a4ab8a1c2946a4fb
David Peterson <peterdav000@warren.k12.in.us>
2024-08-29 13:42:19 -0400

    0000

--- src/java.java
+++ src/java.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class java {
+public class java {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            return temperature >= 25 && temperature <= 45;
+
+        } else {
+            return temperature >= 25 && temperature <= 35;
+        }
+    }
+
+
 }
